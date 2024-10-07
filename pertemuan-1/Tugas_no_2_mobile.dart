90a02a2150289da744463611183cfa6be69be847
Karunia <karunyarizqi@gmail.com>
2024-10-07 11:13:20 +0700

    membuat program yang menggabungkan 3 variabel string pada Tugas_no_2_mobile.dart

--- pertemuan-1/Tugas_no_2_mobile.dart
+++ pertemuan-1/Tugas_no_2_mobile.dart
@@ -1,3 +1,7 @@
 void main(){
-  
+  var str1 = "Halo";
+  var str2 = "Karunia";
+  var str3 = "Informatika";
+
+  print("$str1, perkenalkan nama saya $str2, saya adalah mahasiswa jurursan teknik $str3");
 }
